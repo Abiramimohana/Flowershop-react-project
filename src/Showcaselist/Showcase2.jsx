4fa7c9d4c6fec17b19d71f4c2db132328b9d01cd
Abiramimohana <164880519+Abiramimohana@users.noreply.github.com>
2024-08-07 10:44:17 +0530

    Update Showcase2.jsx

--- src/Showcaselist/Showcase2.jsx
+++ src/Showcaselist/Showcase2.jsx
@@ -5,17 +5,17 @@ import image3 from '../Assest/eventcollection/images (5).jpeg';
 
 export const Showcase2=[
     {
-        name:"lorem",
+        name:"Wedding ",
         image:image1,
         price:100.00
     },
     {
-        name:"lorem",
+        name:"Wedding ",
         image:image2,
         price:100.00
     },
     {
-        name:"lorem",
+        name:"Wedding ",
         image:image3,
         price:100.00
     },
